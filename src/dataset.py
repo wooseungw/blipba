48b467cee7277cb078853e764ecc0793528c0208
wooseungw <woow070840@gmail.com>
2025-05-12 20:24:53 +0900

    주석 처리된 return_assistant_tokens_mask 매개변수 제거

--- src/dataset.py
+++ src/dataset.py
@@ -132,7 +132,7 @@ class VLMDataset(Dataset):
             messages,
             tokenize=True,
             return_tensors="pt",
-            return_assistant_tokens_mask = True,
+            # return_assistant_tokens_mask = True,
             return_dict=True,
         )
         input_ids =  tokenized["input_ids"].squeeze(0)
