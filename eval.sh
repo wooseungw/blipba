82c4ec724365ebd2686b8ae6c6bfe9fd3ffd6704
wooseungw <woow070840@gmail.com>
2025-05-17 20:57:05 +0900

    eval.sh 수정: 모델 경로를 outputs/test/merged_final로 변경

--- eval.sh
+++ eval.sh
@@ -1,7 +1,7 @@
 export CUDA_VISIBLE_DEVICES=0
 export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 
-python evaluation.py --model_path outputs/dinov2-qwen/merged_final \
+python evaluation.py --model_path outputs/test/merged_final \
                        --data_path DATAS/eval/NextQA/formatted_dataset_test.json \
                        --video_root DATAS/eval/NextQA/NExTVideo \
                        --dataset_name NextQA \
