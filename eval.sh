f31dbe8829f4f4eae45e599e999692c10047c4c6
wooseungw <woow070840@gmail.com>
2025-05-17 20:07:34 +0900

    스크립트 수정: eval.sh에서 기본 모델 디렉토리를 outputs/captionvlm/checkpoint-1000에서 outputs/dinov2-qwen/merged_final로 변경

--- eval.sh
+++ eval.sh
@@ -7,7 +7,7 @@ export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 
 # 기본값 설정
 DATASET=${1:-"NextQA"}
-MODEL_DIR=${2:-"outputs/captionvlm/checkpoint-1000"}
+MODEL_DIR=${2:-"outputs/dinov2-qwen/merged_final"}
 VIDEO_ROOT=${3:-"DATAS/eval/NextQA/NExTVideo"}
 RESULTS_DIR="results/captionvlm_eval/${DATASET}"
 DATA_PATH=""
