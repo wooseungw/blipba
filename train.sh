72f2350c2757c13b310a5db3b9db59506aff2878
wooseungw <woow070840@gmail.com>
2025-07-17 10:17:18 +0900

    train_vlm.yaml 삭제 및 train_testing.yaml 추가: 모델 설정 및 학습 구성 업데이트

--- train.sh
+++ train.sh
@@ -2,4 +2,4 @@ export CUDA_VISIBLE_DEVICES=0
 export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 
 
-python train_vlm.py --config config/train.yaml
+python train_vlm.py --config config/train_testing.yaml
