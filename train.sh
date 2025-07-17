33dd7e65abeeb323db4089be37392a5208cbd878
wooseungw <woow070840@gmail.com>
2025-07-17 10:16:21 +0900

    train.yaml 및 train_vlm.yaml 수정: 모델 설정 및 데이터 경로 업데이트, eval.sh 및 train.sh 수정: 평가 및 학습 스크립트 개선

--- train.sh
+++ train.sh
@@ -1,3 +1,5 @@
+export CUDA_VISIBLE_DEVICES=0
+export WANDB_API_KEY=9fd21364ed6c1c6677a250972c5e19a931171974
 
 
-python train.py --config config/train_config.yaml
+python train_vlm.py --config config/train.yaml
